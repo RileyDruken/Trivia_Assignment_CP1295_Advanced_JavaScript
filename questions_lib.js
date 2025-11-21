c3a6e4a57e64ef459b640c6209cc6e33136f626c
riley druken <riley.druken26@ed.cna.nl.ca>
2025-11-21 00:36:45 -0330

    added questions_lib and added question object literal

---
+++ questions_lib.js
@@ -0,0 +1,5 @@
+export let question = {
+    options: [],
+    answer: null,
+    pointValue:null
+}
--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+import { question } from "./questions_lib.js";
+
 document.addEventListener("DOMContentLoaded", (evt) => {
+  let questions = []
+  let exampleQuestion = question;
+  console.log(exampleQuestion)
+
   document.querySelectorAll("#answerList li button").forEach(element => {
     element.addEventListener("click", (evt) => {
         console.log("I was clicked")
